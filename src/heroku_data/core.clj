36767f0d5a5eb5d2d0b85eda5c12deee0684832e
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-05-30 07:52:54 +0200

    let it be

--- src/heroku_data/core.clj
+++ src/heroku_data/core.clj
@@ -5,15 +5,12 @@
 
 (defn get-heroku-data [path options]
 
-  ; TODO -- make these defs into lets (more idiomatic)
-  (def options {:timeout     30000                            ; ms -- 30 seconds!
-                :oauth-token "20ff0356-ca01-48f6-b8da-d6189a33d61b"
-                :headers     {"Accept: application/vnd.heroku+json; version=3"
-                               "Content-type: application/json"}})
-
-  (def api-url "https://api.heroku.com/")
-
-  (let [{:keys [body error] :as resp} @(http/get (str api-url path) options)]
+  (let [options {:timeout     30000                         ; ms -- 30 seconds!
+                 :oauth-token "20ff0356-ca01-48f6-b8da-d6189a33d61b"
+                 :headers     {"Accept: application/vnd.heroku+json; version=3"
+                                "Content-type: application/json"}}
+        api-url "https://api.heroku.com/"
+        {:keys [body error] :as resp} @(http/get (str api-url path) options)]
     (if error
       (println "Failed, exception: " error)
       body)))
