84e70f64c4ff6ba51de4b66904965b469f6c1e24
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-05-30 21:18:20 +0200

    removed references to oauth token (now regenerated) from code

--- src/heroku_data/core.clj
+++ src/heroku_data/core.clj
@@ -2,13 +2,14 @@
   (:require [org.httpkit.client :as http])
   (:require [cheshire.core :refer :all]))
 
+(assert (not-empty (System/getenv "HEROKU_API_TOKEN")))
+
 (defn get-heroku-data [path]
   (let [options {:timeout     30000                         ; ms -- 30 seconds!
-                 :oauth-token "20ff0356-ca01-48f6-b8da-d6189a33d61b"
+                 :oauth-token (System/getenv "HEROKU_API_TOKEN")
                  :headers     {"Accept: application/vnd.heroku+json; version=3"
                                 "Content-type: application/json"}}
-        api-url "https://api.heroku.com/"
-        {:keys [body error] :as resp} @(http/get (str api-url path) options)]
+        {:keys [body error]} @(http/get (str "https://api.heroku.com/" path) options)]
     (if error
       (println "Failed, exception: " error)
       (parse-string body true))))
@@ -17,20 +18,14 @@
 
 (def orgs (get-heroku-data org-path))
 
-(def apps-per-orgs (map #(get-heroku-data (str org-path (:name %) "/apps")) orgs))
-
-; apply concat to peel away any enclosing sequences
-(def app-names (map #(select-keys % [:name :organization]) (apply concat apps-per-orgs)))
+(def apps-per-orgs (apply concat (map #(get-heroku-data (str org-path (:name %) "/apps")) orgs)))
 
 (defn get-app-env-vars [app]
   (let [env-vars (get-heroku-data (str "/apps/" (:name app) "/config-vars"))]
     (vector app env-vars)))
 
-(defn env-app-vars [] (map #(get-app-env-vars %) app-names))
+; this gets all environments, for all apps for all orgs
+(defn org-app-env [] (map #(get-app-env-vars %) apps-per-orgs))
 
 ; TODO ... store it in mongo for further queries
-
-
-
-
 
