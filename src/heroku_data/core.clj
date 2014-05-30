1888834d3c0d8f1371c1961f82d23fcb21970658
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-05-30 17:36:59 +0200

    tidied some more

--- src/heroku_data/core.clj
+++ src/heroku_data/core.clj
@@ -13,8 +13,6 @@
       (println "Failed, exception: " error)
       (parse-string body true))))
 
-(def orgs (get-heroku-data org-path))
-
 (def org-path "/organizations/")
 
 (def orgs (get-heroku-data org-path))
@@ -24,19 +22,12 @@
 ; apply concat to peel away any enclosing sequences
 (def app-names (map #(select-keys % [:name :organization]) (apply concat apps-per-orgs)))
 
-; TODO -- return the app with the vars
 (defn get-app-env-vars [app]
-  (get-heroku-data (str "/apps/" (:name app) "/config-vars")))
+  (let [env-vars (get-heroku-data (str "/apps/" (:name app) "/config-vars"))]
+    (vector app env-vars)))
 
 (defn env-app-vars [] (map #(get-app-env-vars %) app-names))
 
-(def some-json "{\"MONGOLAB_URI\":\"mongodb://heroku_app22930768_A:KKgliGwPIzbQkMZmWSdsVkFczXcybksX@ds027280-a0.mongolab.com:27280,ds027280-a1.mongolab.com:27280/heroku_app22930768\",\"NEW_RELIC_LICENSE_KEY\":\"7098d10fbf70572dc7da6736964b9b77e49ac164\",\"NEW_RELIC_LOG\":\"stdout\",\"LOADERIO_API_KEY\":\"8ace9ef99a8946bb01c2e40c6f852557\"}")
-
-
-
-; TODO add another JSON entry with the app name to the front via a fn
-
-; TODO need to combine the env-vars with the app / org data
 ; TODO ... store it in mongo for further queries
 
 
