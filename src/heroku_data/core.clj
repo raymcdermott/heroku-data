6a6e62f0cc582302ea204c698619d04d7417989b
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-05-30 07:49:11 +0200

    a little data hiding action

--- src/heroku_data/core.clj
+++ src/heroku_data/core.clj
@@ -3,20 +3,22 @@
   (:require [cheshire.core :refer :all]))
 
 
-(defn get-heroku-data [url options]
-  (let [{:keys [body error] :as resp} @(http/get url options)]
+(defn get-heroku-data [path options]
+
+  ; TODO -- make these defs into lets (more idiomatic)
+  (def options {:timeout     30000                            ; ms -- 30 seconds!
+                :oauth-token "20ff0356-ca01-48f6-b8da-d6189a33d61b"
+                :headers     {"Accept: application/vnd.heroku+json; version=3"
+                               "Content-type: application/json"}})
+
+  (def api-url "https://api.heroku.com/")
+
+  (let [{:keys [body error] :as resp} @(http/get (str api-url path) options)]
     (if error
       (println "Failed, exception: " error)
       body)))
 
-(def options {:timeout     30000                            ; ms -- 30 seconds!
-              :oauth-token "20ff0356-ca01-48f6-b8da-d6189a33d61b"
-              :headers     {"Accept: application/vnd.heroku+json; version=3"
-                             "Content-type: application/json"}})
-
-(def api-url "https://api.heroku.com/")
-
-(def org-url (str api-url "/organizations/"))
+(def org-url "/organizations/")
 
 (def orgs (:name (parse-string (get-heroku-data org-url options) true)))
 
@@ -27,7 +29,7 @@
 ; apply concat to peel away any enclosing sequences
 (def app-names (map #(select-keys % [:name]) (apply concat apps-per-orgs)))
 
-(def env-app-vars (json2map (map #(get-heroku-data (str api-url "/apps/" % "/config-vars") options)
+(def env-app-vars (json2map (map #(get-heroku-data (str "/apps/" % "/config-vars") options)
                                  (map :name app-names))))
 
 (def some-json "{\"MONGOLAB_URI\":\"mongodb://heroku_app22930768_A:KKgliGwPIzbQkMZmWSdsVkFczXcybksX@ds027280-a0.mongolab.com:27280,ds027280-a1.mongolab.com:27280/heroku_app22930768\",\"NEW_RELIC_LICENSE_KEY\":\"7098d10fbf70572dc7da6736964b9b77e49ac164\",\"NEW_RELIC_LOG\":\"stdout\",\"LOADERIO_API_KEY\":\"8ace9ef99a8946bb01c2e40c6f852557\"}")
