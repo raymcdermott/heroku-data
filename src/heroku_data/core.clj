0ebc9dc224e842cb1f3fd916b807ae6cbc42b3cd
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-06-01 22:19:50 +0200

    cleaned up def / defn / let ... all functions now

--- src/heroku_data/core.clj
+++ src/heroku_data/core.clj
@@ -2,10 +2,10 @@
   (:require [org.httpkit.client :as http])
   (:require [cheshire.core :refer :all])
   (:require [monger.core :as mg]
-            [monger.collection :as mc])
-  (:import org.bson.types.ObjectId))
+            [monger.collection :as mc]))
 
 (assert (not-empty (System/getenv "HEROKU_API_TOKEN")))
+(assert (not-empty (System/getenv "MONGO_URL")))
 
 (defn get-heroku-data [path]
   (let [options {:timeout     30000                         ; ms -- 30 seconds!
@@ -17,11 +17,12 @@
       (println "Failed, exception: " error)
       (parse-string body true))))
 
-(def org-path "/organizations/")
+(defn apps-per-org [organization]
+  (get-heroku-data (str "/organizations/" organization "/apps")))
 
-(def orgs (get-heroku-data org-path))
-
-(def apps-per-orgs (apply concat (map #(get-heroku-data (str org-path (:name %) "/apps")) orgs)))
+(defn apps-all-orgs []
+  (let [orgs (get-heroku-data "/organizations/")]
+    (apply concat (map #(apps-per-org (:name %)) orgs))))
 
 (defn get-app-env-vars [app]
   (let [env-vars (get-heroku-data (str "/apps/" (:name app) "/config-vars"))]
@@ -35,6 +36,6 @@
 
 (defn org-app-env []
   "Gets config data, for all apps for all orgs and saves it (per app) to mongoDB"
-  (let [data (map #(get-app-env-vars %) apps-per-orgs)]
+  (let [apps (apps-all-orgs)
+        data (map #(get-app-env-vars %) apps)]
     (map save-to-mongo data)))
-
