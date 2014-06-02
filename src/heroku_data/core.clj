80cc1127dd688e12f059f9f0a7df1a22b2a20ceb
Ray McDermott <ray.mcdermott@toyota-europe.com>
2014-06-02 17:00:19 +0200

    env diffing

--- src/heroku_data/core.clj
+++ src/heroku_data/core.clj
@@ -1,11 +1,12 @@
 (ns heroku-data.core
+  (:require [clojure.set :as s])
   (:require [org.httpkit.client :as http])
   (:require [cheshire.core :refer :all])
   (:require [monger.core :as mg]
             [monger.collection :as mc]))
 
 (assert (not-empty (System/getenv "HEROKU_API_TOKEN")))
-(assert (not-empty (System/getenv "MONGO_URL")))
+;(assert (not-empty (System/getenv "MONGO_URL")))
 
 (defn get-heroku-data [path]
   (let [options {:timeout     30000                         ; ms -- 30 seconds!
@@ -28,14 +29,41 @@
   (let [env-vars (get-heroku-data (str "/apps/" (:name app) "/config-vars"))]
     (hash-map :app app :env env-vars)))
 
+(defn get-env-vars [app-name]
+  (let [env-vars (get-heroku-data (str "/apps/" app-name "/config-vars"))]
+    (hash-map :app app-name :env env-vars)))
+
 (defn save-to-mongo [configuration-data]
   (let [uri (System/getenv "MONGO_URL")
         {:keys [conn db]} (mg/connect-via-uri uri)]
     (mc/insert-and-return db "orgAppEnv" configuration-data)
     (mg/disconnect conn)))
 
-(defn org-app-env []
+(defn all-org-app-env []
   "Gets config data, for all apps for all orgs and saves it (per app) to mongoDB"
   (let [apps (apps-all-orgs)
         data (map #(get-app-env-vars %) apps)]
     (map save-to-mongo data)))
+
+(defn org-app-env [organization]
+  "Gets config data, for all apps for the given organization"
+  (let [apps (apps-per-org organization)
+        data (map #(get-app-env-vars %) apps)]
+    data))
+
+(defn forbidden-apps [apps]
+  (filter #(= "forbidden" (get-in % [:env :id])) apps))
+
+(defn allowed-apps [organization]
+  (let [org-apps (set (apps-per-org organization))
+        forbidden (set (forbidden-apps org-apps))]
+    (s/difference org-apps forbidden)))
+
+(defn env-variance [app-to-compare & other-apps]
+  (let [from-app (get-env-vars app-to-compare)
+        comparative-apps (map #(get-env-vars %) other-apps)
+        from-env (set (:env from-app))
+        sample-env (set (:env (first comparative-apps)))]
+    (pprint (map #(println (str "Comparing " from-app  " to " )
+                   s/difference (set (:env %)) from-env) comparative-apps))))
+
